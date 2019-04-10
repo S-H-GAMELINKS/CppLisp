0840d3da21865fcb7ba6674c8280e4aabae670ec
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-10 18:23:43 +0900

    add interpreter for CppLisp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@
 
 int main() {
 
+    std::string stdin;
+
     // Print CppLisp Version
     std::cout << "CppLisp Version 0.0.0.1" << std::endl;
     std::cout << "Press Ctrl + C to Exit" << std::endl;
+
+    while(true) {
+        std::cout << "CppLisp >> ";
+        std::cin >> stdin;
+        std::cout << stdin << std::endl;
+    }
 
     return 0;
 }
