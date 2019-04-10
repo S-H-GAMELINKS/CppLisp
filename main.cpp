8d2adbcfefb65e6c73792fb75d8926f6c17564c5
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-10 18:14:32 +0900

    add main.cpp & ignore ipch

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    // Print CppLisp Version
+    std::cout << "CppLisp Version 0.0.0.1" << std::endl;
+    std::cout << "Press Ctrl + C to Exit" << std::endl;
+
+    return 0;
+}
